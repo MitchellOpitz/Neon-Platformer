bb55afbd8d79acc9178461403dcc8eb969210a28
Mitchell Opitz <mitchell@mitchellopitz.net>
2022-07-11 00:53:42 -0500

    feat - Add abstract behavior class to player
    When creating player actions (walk, run, jump, etc.), each action will be self-contained.  All actions will extend this single abstract behavior class.

---
+++ Assets/Scripts/AbstractBehavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractBehavior : MonoBehaviour
+{
+
+    public Buttons[] inputButtons;
+
+    protected InputState inputState;
+    protected Rigidbody2D body2d;
+
+    protected virtual void Awake()
+    {
+        inputState = GetComponent<InputState>();
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
