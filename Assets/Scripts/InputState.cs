bc6903cd600dad2ec6ea9145bbcf79240460257a
Mitchell Opitz <mitchell@mitchellopitz.net>
2022-07-11 00:35:10 -0500

    fix - Remove console log messages from input

--- Assets/Scripts/InputState.cs
+++ Assets/Scripts/InputState.cs
@@ -32,12 +32,12 @@ public class InputState : MonoBehaviour
 
         if (state.value && !value)
         {
-            Debug.Log("Button " + key + " released " + state.holdTime);
+            //Debug.Log("Button " + key + " released " + state.holdTime);
             state.holdTime = 0;
         } else if (state.value && value )
         {
             state.holdTime += Time.deltaTime;
-            Debug.Log("Button " + key + " down " + state.holdTime);
+            //Debug.Log("Button " + key + " down " + state.holdTime);
         }
 
         state.value = value;
