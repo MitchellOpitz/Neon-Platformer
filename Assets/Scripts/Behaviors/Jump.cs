2a59c6fd3a9abd1e1b145f96ae5160584edd8abe
Mitchell Opitz <mitchell@mitchellopitz.net>
2022-07-13 22:46:56 -0500

    feat - Add double jump and long jump

--- Assets/Scripts/Behaviors/Jump.cs
+++ Assets/Scripts/Behaviors/Jump.cs
@@ -6,6 +6,12 @@ public class Jump : AbstractBehavior
 {
 
     public float jumpSpeed = 200f;
+    public float jumpDelay = 0.1f;
+    public int jumpCount = 2;
+    public GameObject jumpEffectPrefab;
+
+    protected float lastJumpTime = 0;
+    protected int jumpsRemaining = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +20,7 @@ public class Jump : AbstractBehavior
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         var canJump = inputState.GetButtonValue(inputButtons[0]);
         var holdTime = inputState.GetButtonHoldTime(inputButtons[0]);
@@ -23,7 +29,20 @@ public class Jump : AbstractBehavior
         {
             if (canJump && holdTime < 0.1f)
             {
+                jumpsRemaining = jumpCount - 1;
                 OnJump();
+            }
+        } else
+        {
+            if (canJump && holdTime < 0.1f && Time.time - lastJumpTime > jumpDelay)
+            {
+                if(jumpsRemaining > 0)
+                {
+                    jumpsRemaining --;
+                    OnJump();
+                    var clone = Instantiate(jumpEffectPrefab);
+                    clone.transform.position = transform.position;
+                }
             }
         }
     }
@@ -31,6 +50,7 @@ public class Jump : AbstractBehavior
     protected virtual void OnJump()
     {
         var vel = body2d.velocity;
+        lastJumpTime = Time.time;
         body2d.velocity = new Vector2(vel.x, jumpSpeed);
     }
 }
